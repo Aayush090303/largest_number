2ebaa351924d3519ebd24dd3a82754a19f81269e
Aayush090303 <93429350+Aayush090303@users.noreply.github.com>
2023-12-11 17:36:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ def find_largest(num1,num2,num3):
 
 st.title('find the largest number')
 
-num1 = st.number_input('Enter first number' value==0.0)
-num2 = st.number_input('Enter second number' value==0.0)
-num3 = st.number_input('Enter third number' value==0.0)
+num1 = st.number_input('Enter first number', value==0.0)
+num2 = st.number_input('Enter second number', value==0.0)
+num3 = st.number_input('Enter third number', value==0.0)
 
 if st.button('find largest'):
   largest = find_largest(num1,num2,num3)
